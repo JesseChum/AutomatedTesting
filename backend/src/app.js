2c23d517dfe536ba53ba7a4a0d6b823f7172524a
timdwms <timmywilliams4665@gmail.com>
2026-02-23 21:01:20 -0800

    Got project running

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import todoRoutes from "./routes/todos.js";
+import todoRoutes from "./routes/todo.js";
 
 const app = express();
 
--- frontend/Automated-Testing/src/main.jsx
+++ frontend/Automated-Testing/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import TodoForm from './TodoForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodoForm onAdd={(title) => console.log(title)} />
   </StrictMode>,
 )
